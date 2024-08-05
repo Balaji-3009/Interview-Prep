016593f52ab03ecf5558e6d03ae477f2a7b8011d
Balaji-3009 <mbalaji3009@gmail.com>
2024-08-05 19:45:58 +0530

    integrated upload pdf

--- api/routes.py
+++ api/routes.py
@@ -5,7 +5,21 @@ from database.session import db_dependency
 from pdf_to_json import pdfreader
 from .mistral import query
 import json
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
+app = FastAPI()
+
+app.mount("/assets", StaticFiles(directory="assets"), name="assets")
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/items/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse(
+        request=request, name="homepage.html", context={"id": id}
+    )
 
 router = APIRouter()
 
@@ -13,8 +27,12 @@ UPLOAD_DIRECTORY = "uploaded_pdfs"
 
 Path(UPLOAD_DIRECTORY).mkdir(parents=True, exist_ok=True)
 
-@router.post("/upload-pdf/")
-async def upload_pdf(file: UploadFile = File(...)):
+@router.get("/pdf", response_class=HTMLResponse)
+async def get_upload_form(request: Request):
+    return templates.TemplateResponse("homepage.html", {"request": request})
+
+@router.post("/upload-pdf/", response_class=HTMLResponse)
+async def upload_pdf(request: Request, file: UploadFile = File(...)):
     if file.content_type != "application/pdf":
         raise HTTPException(status_code=400, detail="Invalid file type. Only PDFs are allowed.")
     
@@ -26,56 +44,38 @@ async def upload_pdf(file: UploadFile = File(...)):
     except Exception as e:
         raise HTTPException(status_code=500, detail="Failed to save file")
     
-    input = file_path
-    pdfreader(input,"output.txt")
-    f = open("output.txt", "r")
-    content = f.read()
-
+    pdfreader(file_path, "output.txt")
     
-    output1 = query({
-        "inputs": """Functional Resume Sample """ + content + """ From this resume give a list of interview questions considering you are an interviewer. Only take into account the projects done. Give me 3 questions.""",
-        "parameters": {
-            "return_full_text": False
-        }
-    }) 
+    with open("output.txt", "r") as f:
+        content = f.read()
 
-    output2 = query({
-        "inputs": """Functional Resume Sample """ + content + """ From this resume give a list of interview questions considering you are an interviewer. Only take into account the work experience. Give me 3 questions.""",
-        "parameters": {
-            "return_full_text": False
-        }
-    })
-    
-    output3 = query({
-        "inputs": """Functional Resume Sample """ + content + """ From this resume give a list of interview questions considering you are an interviewer. Give question from sections except projects and work experience. Give me 2 properly framed questions. This can include their clubs and chapters, publications, extracurricular activities or any other relevant fields""",
-        "parameters": {
-            "return_full_text": False
-        }
-    })
+    questions = []
+    queries = [
+        {
+            "inputs": f"Functional Resume Sample {content} From this resume give a list of interview questions considering you are an interviewer. Only take into account the projects done. Give me 3 questions.",
+            "parameters": {"return_full_text": False},
+        },
+        {
+            "inputs": f"Functional Resume Sample {content} From this resume give a list of interview questions considering you are an interviewer. Only take into account the work experience. Give me 3 questions.",
+            "parameters": {"return_full_text": False},
+        },
+        {
+            "inputs": f"Functional Resume Sample {content} From this resume give a list of interview questions considering you are an interviewer. Give questions from sections except projects and work experience. Give me 2 properly framed questions. This can include their clubs and chapters, publications, extracurricular activities or any other relevant fields.",
+            "parameters": {"return_full_text": False},
+        },
+    ]
 
-    output = []
+    for query_data in queries:
+        response = query(query_data)
+        for qns in response:
+            out = qns["generated_text"].split('\n')
+            for i in out:
+                if len(i) > 5:
+                    questions.append(i[2:])
 
-    for qns in output1:
-        out=(qns["generated_text"].split('\n'))
-        for i in out:
-            if len(i)>5:
-                output.append(i[2::])
-                
-        
-    for qns in output2:
-        out=(qns["generated_text"].split('\n'))
-        for i in out:
-            if len(i)>5:
-                output.append(i[2::])
-        
-    for qns in output3:
-        out=(qns["generated_text"].split('\n'))
-        for i in out:
-            if len(i)>5:
-                output.append(i[2::])
-        
-    return output
-
+    return templates.TemplateResponse(
+        "homepage.html", {"request": request, "questions": questions}
+    )
 
 
 
