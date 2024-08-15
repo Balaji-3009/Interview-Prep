c7e2d9fc2c89ef74757ccbdbe7cfe0fd69bfc94d
Balaji-3009 <mbalaji3009@gmail.com>
2024-08-15 22:15:15 +0530

    minor fixes

--- api/mistral.py
+++ api/mistral.py
@@ -2,6 +2,7 @@ import requests
 from dotenv import load_dotenv
 import os
 
+load_dotenv('.env')
 hugging_face_api_key = os.getenv("HUGGING_FACE_API_KEY")
 
 API_URL = "https://api-inference.huggingface.co/models/mistralai/Mixtral-8x7B-Instruct-v0.1"
