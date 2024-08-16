d30d93aa9e6986c4b1f137c9028d2f347907d087
Balaji-3009 <mbalaji3009@gmail.com>
2024-08-16 16:03:39 +0530

    integrated home page

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import JSONResponse
 from api.routes import router
 from fastapi.staticfiles import StaticFiles
-
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from api.routes import get_upload_form
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
 
 app.include_router(router, prefix="/api", tags=["api"])
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"} 
+@app.get("/",response_class = HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("index.html",{"request": request}) 
